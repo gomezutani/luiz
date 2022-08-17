c47c054dfccedeb26de9ef44f77b9e2fa1ed50ce
luizito <you@example.com>
2022-08-17 01:50:00 -0500

    subiendo trabajo de sistema

---
+++ procesos/holamundo.c
@@ -0,0 +1,7 @@
+#include <stadio.h>
+int main()
+{
+ printf( "Hola mundo. ");
+ getch();
+ return 0;
+}
---
+++ procesos/proceso.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+    pid_d id_proceso_padre
+
+    puts("\n==== ESTE PROGARAMA EL IDENTIFICADOR DE UN PROCESO PADRE ====\n")
+
+    id_proceso_padre = getppid();
+
+    printf("Identificador del proceso padre : %d\n" , id_proceso_padre);
+
+    return 0;
+
+}
